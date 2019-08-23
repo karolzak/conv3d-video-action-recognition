830756a1b592848885a990ba85f6daba02a9ada8
karolzak <karol.zak@hotmail.com>
2019-08-23 20:53:23 +0000

    added step to extract_clip function and added crop_center func

--- python/data_prep.py
+++ python/data_prep.py
@@ -10,27 +10,28 @@ def random_frames(video, target_num_frames):
     return video[frames_idxs]
 
 
-def random_crop(img, mask=None, width=112, height=112, x=None, y=None):
-    #assert img.shape[0] >= height
-    #assert img.shape[1] >= width
+def random_crop(img, width=112, height=112, x=None, y=None):
     if x is None:
         x = random.randint(0, img.shape[1] - width)
     if y is None:
         y = random.randint(0, img.shape[0] - height)
     img = img[y:y+height, x:x+width]
-    if mask is None:
-        return img
-    else:
-        #assert img.shape[0] == mask.shape[0]
-        #assert img.shape[1] == mask.shape[1]
-        mask = mask[y:y+height, x:x+width]
-        return img, mask
+    return img
 
 
-def extract_clips(video, frames_per_clip=16):
+def crop_center(img, width=112, height=112):
+    x = (img.shape[1] - width) // 2
+    y = (img.shape[0] - height) // 2
+    img = img[y:y+height, x:x+width]
+    return img
+
+
+def extract_clips(video, frames_per_clip=16, step=None):
+    if step is None:
+        step = frames_per_clip
     total_frames = video.shape[0]
-    num_clips = total_frames // frames_per_clip
-    extracted_clips = [np.asarray(video[i:i+frames_per_clip]) for i in range(0,frames_per_clip*num_clips,frames_per_clip)]
+    assert frames_per_clip <= total_frames
+    extracted_clips = [np.asarray(video[i:i+frames_per_clip]) for i in range(0, total_frames-frames_per_clip, step)]
     
     return np.asarray(extracted_clips, dtype=video.dtype)
 
@@ -67,7 +68,7 @@ def calculate_mean_std(x, channels_first=False, verbose=0):
     return np.stack((all_mean, all_std))
 
 
-def preprocess_input(x, mean_std, channels_first=False, verbose=0):
+def preprocess_input(x, mean_std, divide_std=False, channels_first=False, verbose=0):
     x = np.asarray(x, dtype=np.float32)    
     ndim = x.ndim
     assert ndim in [5,4]
@@ -77,13 +78,15 @@ def preprocess_input(x, mean_std, channels_first=False, verbose=0):
     for c in range(0, num_channels):  
         if ndim ==5: # videos
             x[:,:,:,:,c] -= mean_std[0][c]
-            x[:,:,:,:,c] /= mean_std[1][c]
+            if divide_std:
+                x[:,:,:,:,c] /= mean_std[1][c]
             if verbose:
                 print("Channel %s mean after preprocessing: %s" % (c, x[:,:,:,:,c].mean()))    
                 print("Channel %s std after preprocessing: %s" % (c, x[:,:,:,:,c].std()))
         elif ndim ==4: # images rgb or grayscale
             x[:,:,:,c] -= mean_std[0][c]
-            x[:,:,:,c] /= mean_std[1][c]   
+            if divide_std:
+                x[:,:,:,c] /= mean_std[1][c]   
             if verbose:        
                 print("Channel %s mean after preprocessing: %s" % (c, x[:,:,:,c].mean()))    
                 print("Channel %s std after preprocessing: %s" % (c, x[:,:,:,c].std()))
