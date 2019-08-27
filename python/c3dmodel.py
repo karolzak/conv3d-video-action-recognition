7698d9136a154afc1ab620ff0a15f3a4efe05999
karolzak <karol.zak@hotmail.com>
2019-08-27 14:05:51 +0000

    :pencil: added comments to c3dmodel.py

--- python/c3dmodel.py
+++ python/c3dmodel.py
@@ -5,15 +5,23 @@ from keras.optimizers import SGD
 import keras.backend as K
 
 def C3D_model(weights_path=None, summary=False, trainable=True, num_layers_remove=0):
-    """ Create Keras model of C3D model with an option to load pretrained weights trained on Sports1M dataset
-
-    arguments:
-        weights_path (str):  (default=None)
-        summary (bool):  (default=False)
-        trainable (bool):  (default=True)
-        num_layers_remove (int):  (default=0)
-
-    return: 
+    """
+    Create Keras model of C3D model with an option to load pretrained weights trained on Sports1M dataset
+    
+    Parameters
+    ----------
+    weights_path : str, optional
+        Path to the model file with pretrained weights, by default None
+    summary : bool, optional
+        To print a model summary or not, by default False
+    trainable : bool, optional
+        Sets all the layers as trainable, by default True
+    num_layers_remove : int, optional
+        How many layers to pop from the top of the network, by default 0
+    
+    Returns
+    -------
+    Keras model
         Keras model of C3D network
     """
 
@@ -84,6 +92,19 @@ def C3D_model(weights_path=None, summary=False, trainable=True, num_layers_remov
 
 
 def get_video_descriptor(weights_path='../models/weights_C3D_sports1M_tf.h5'):
+    """
+    Function that creates a C3D model for features generation purpose
+    
+    Parameters
+    ----------
+    weights_path : str, optional
+        Path to the model file with pretrained weights, by default '../models/weights_C3D_sports1M_tf.h5'
+    
+    Returns
+    -------
+    Keras model
+        Keras model that can be used as video descriptor to generate features
+    """
     model = C3D_model(weights_path=weights_path, num_layers_remove=3)
     model.add(Lambda(lambda  x: K.l2_normalize(x, axis=1)))
     return model
